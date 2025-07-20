e3bf3a1c964d9e15d0fe72195a4cb2ee6c6d3f84
LMBarboza <leon.uchoa@gmail.com>
2025-07-20 17:24:19 -0300

    add include file

---
+++ compgeo/compgeo.hpp
@@ -0,0 +1,18 @@
+#ifndef COMPGEO_HPP
+#define COMPGEO_HPP
+
+#include "geometry/edge.hpp"
+#include "geometry/point.hpp"
+#include "geometry/polygon.hpp"
+#include "geometry/triangle.hpp"
+
+#include "algorithms/convex_hull/convex_hull.hpp"
+#include "algorithms/convex_hull/convex_hull_divide_and_conquer.hpp"
+#include "algorithms/convex_hull/convex_hull_gift_wrapping.hpp"
+#include "algorithms/convex_hull/convex_hull_graham_scan.hpp"
+#include "algorithms/convex_hull/convex_hull_incremental.hpp"
+
+#include "algorithms/triangulation/tags.hpp"
+#include "algorithms/triangulation/triangulator.hpp"
+
+#endif
