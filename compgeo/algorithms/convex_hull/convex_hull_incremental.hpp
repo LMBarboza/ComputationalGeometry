10a6d85f9c0a88bedeb2fcd4c6ba2af68cfc5d3d
LMBarboza <leon.uchoa@gmail.com>
2025-07-20 20:14:26 -0300

    add incremental algorithm

--- compgeo/algorithms/convex_hull/convex_hull_incremental.hpp
+++ compgeo/algorithms/convex_hull/convex_hull_incremental.hpp
@@ -2,6 +2,7 @@
 
 #include "../../geometry/point.hpp"
 #include "convex_hull.hpp"
+#include <algorithm>
 #include <vector>
 
 namespace compgeo {
@@ -10,12 +11,35 @@ namespace convex_hull {
 
 template <> struct ConvexHullAlgorithm<IncrementalTag> {
   static std::vector<geometry::Point>
-  compute(const std::vector<geometry::Point> &points, IncrementalTag) {
-    return points;
-  }
-}
+  compute(std::vector<geometry::Point> &points, IncrementalTag) {
+    std::sort(points.begin(), points.end());
+    if (points.size() < 3)
+      return points;
 
-;
+    std::vector<geometry::Point> hull(points.begin(), points.begin() + 3);
+
+    for (size_t i = 3; i < points.size(); ++i) {
+      hull.push_back(points[i]);
+
+      bool changed = true;
+      while (changed) {
+        changed = false;
+        for (size_t j = 0; j < hull.size(); ++j) {
+          size_t prev = (j == 0) ? hull.size() - 1 : j - 1;
+          size_t next = (j + 1) % hull.size();
+
+          if (geometry::Point::cross(hull[prev], hull[j], hull[next]) <= 0) {
+            hull.erase(hull.begin() + j);
+            changed = true;
+            break;
+          }
+        }
+      }
+    }
+
+    return hull;
+  }
+};
 } // namespace convex_hull
 
 } // namespace algorithms
