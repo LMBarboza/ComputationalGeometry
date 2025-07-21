fe4c36a347bbc92f39317eee411add2a5ede8f29
LMBarboza <leon.uchoa@gmail.com>
2025-07-20 21:44:41 -0300

    add gift wrapping

--- compgeo/algorithms/convex_hull/convex_hull_gift_wrapping.hpp
+++ compgeo/algorithms/convex_hull/convex_hull_gift_wrapping.hpp
@@ -10,8 +10,35 @@ namespace convex_hull {
 
 template <> struct ConvexHullAlgorithm<GiftWrappingTag> {
   static std::vector<geometry::Point>
-  compute(const std::vector<geometry::Point> &points, GiftWrappingTag) {
-    return points;
+  compute(std::vector<geometry::Point> &points, GiftWrappingTag) {
+    std::vector<geometry::Point> hull;
+    if (points.size() < 3)
+      return points;
+
+    int leftmost = 0;
+    for (int i = 1; i < points.size(); ++i) {
+      if (points[i].x < points[leftmost].x ||
+          (points[i].x == points[leftmost].x &&
+           points[i].y < points[leftmost].y)) {
+        leftmost = i;
+      }
+    }
+
+    int p = leftmost;
+    do {
+      hull.push_back(points[p]);
+      int next = (p + 1) % points.size();
+
+      for (int i = 0; i < points.size(); ++i) {
+        if (geometry::Point::cross(points[p], points[next], points[i]) < 0) {
+          next = i;
+        }
+      }
+
+      p = next;
+    } while (p != leftmost);
+
+    return hull;
   }
 }
 
